38cd1d199e52cf2cd209155f1bc6438151113e39
pushgive19 <59918189+pushgive19@users.noreply.github.com>
2020-02-17 23:10:13 +0900

    02.16 lecture

---
+++ theory/1.5HelloWorld/index.js
@@ -0,0 +1,2 @@
+alert("나 JavaScript야 잘작동해 나멋지지? 굿?");
+console.log("나 JavaScript야 잘작동해 나멋지지? 굿?");
---
+++ theory/1.6/index.js
@@ -0,0 +1,10 @@
+// variable
+// 1. create
+// 2. initialize
+// 3. use
+
+let a = 221;
+let b = a - 5;
+console.log(b, a);
+
+// 변수 선언 let
