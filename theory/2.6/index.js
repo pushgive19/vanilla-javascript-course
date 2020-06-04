90c2854e126358b446f9f6f0d3eea643c426bbce
pushgive19 <59918189+pushgive19@users.noreply.github.com>
2020-06-04 23:14:40 +0900

    tutorial_review&&myOwn

--- theory/2.6/index.js
+++ theory/2.6/index.js
@@ -1,28 +1,55 @@
+// const title = document.querySelector("#title");
+
+// const BASE_COLOR = "rgb(44, 62, 80)";
+// const OTHER_COLOR = "rgb(236, 240, 241)";
+
+// function handleClick() {
+//   // event는 어디서 온거여?
+//   //   console.log(event);
+//   //   console.log("I have benn resized");
+//   // console.log(title.style.color);
+//   const currentColor = title.style.color;
+//   console.log(currentColor);
+//   if (currentColor === BASE_COLOR) {
+//     title.style.color = OTHER_COLOR;
+//   } else {
+//     title.style.color = BASE_COLOR;
+//   }
+// }
+
+// function init() {
+//   //application 초기화
+//   title.style.color = BASE_COLOR;
+//   title.addEventListener("click", handleClick);
+// }
+// init();
+
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(44, 62, 80)";
-const OTHER_COLOR = "rgb(236, 240, 241)";
+const BASE_COLOR = "rgb(162, 155, 254)";
+const OTHER_COLOR = "#74b9ff";
 
-function handleClick() {
-  // event는 어디서 온거여?
-  //   console.log(event);
-  //   console.log("I have benn resized");
-  // console.log(title.style.color);
+title.style.color = BASE_COLOR;
+
+  
+function handleClick(){
   const currentColor = title.style.color;
-  console.log(currentColor);
-  if (currentColor === BASE_COLOR) {
+  if(currentColor === BASE_COLOR){
     title.style.color = OTHER_COLOR;
-  } else {
+  }else{
     title.style.color = BASE_COLOR;
   }
-}
+}  
 
-function init() {
-  //application 초기화
-  title.style.color = BASE_COLOR;
-  title.addEventListener("click", handleClick);
+function init(){
+    title.addEventListener("wheel", handleClick);
 }
 init();
 
-
+// mouseout	A pointing device is moved off the element that has the listener attached or off one of its children.
+// mouseup	A pointing device button is released over an element.
+// pointerlockchange	The pointer was locked or released.
+// pointerlockerror	It was impossible to lock the pointer for technical reasons or because the permission was denied.
+// select	Some text is being selected.
+// wheel	A wheel button of a pointing device is rotated in any direction.
 
--- theory/2.7DOM/index.js
+++ theory/2.7DOM/index.js
@@ -1,37 +1,56 @@
+// const title = document.querySelector("#title");
+
+// const CLICKED_CLASS = 'clicked';
+// const UNCLICKED_CLASS = 'unclicked';
+
+// function handleClick(){
+//   // const hasClass = title.classList.contains(CLICKED_CLASS);
+//   // if(hasClass ){
+//   //   title.classList.remove(CLICKED_CLASS);
+//   // }else{
+//   //   title.classList.add(CLICKED_CLASS);
+//   // }
+//   title.classList.toggle(CLICKED_CLASS);
+  
+// }
+// function handleEnter(){
+
+//   title.classList.replace(CLICKED_CLASS,UNCLICKED_CLASS );
+//   title.classList.toggle(UNCLICKED_CLASS);title.classList.contains(CLICK_CLASS)
+//   console.log(title.classList);
+// }
+
+
+// function init() {
+
+//   title.addEventListener("click", handleClick);
+// }
+// init();
+
+// function init2(){
+//   title.addEventListener("mouseenter",handleEnter );
+
+// }
+// init2();
+
+  
 const title = document.querySelector("#title");
 
-const CLICKED_CLASS = 'clicked';
-const UNCLICKED_CLASS = 'unclicked';
+const CLICK_CLASS= "clicked";
 
 function handleClick(){
-  // const hasClass = title.classList.contains(CLICKED_CLASS);
-  // if(hasClass ){
-  //   title.classList.remove(CLICKED_CLASS);
+  // const currentClass = title.className;
+  // const contain = title.classList.contains(CLICK_CLASS);
+  // if(contain){
+  //   title.classList.remove(CLICK_CLASS);
   // }else{
-  //   title.classList.add(CLICKED_CLASS);
+  //   title.classList.add(CLICK_CLASS);
   // }
-  title.classList.toggle(CLICKED_CLASS);
-  
-}
-function handleEnter(){
-
-  title.classList.replace(CLICKED_CLASS,UNCLICKED_CLASS );
-  title.classList.toggle(UNCLICKED_CLASS);
-  console.log(title.classList);
+  title.classList.toggle(CLICK_CLASS);
 }
 
-
-function init() {
-
+function init(){
   title.addEventListener("click", handleClick);
+
 }
 init();
-
-function init2(){
-  title.addEventListener("mouseenter",handleEnter );
-
-}
-init2();
-
-  
-
